e041b9e740cec9b22fba0db5ee6fe0019a0b82cc
lcranfrd <lcranfrd@comcast.net>
2021-04-05 16:10:43 -0400

    updates

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const express = require('express');
 const app = express();
 
 app.use('/static', express.static('public'));
-app.use('/fontawesome', express.static('node_modules/@fortawesome/fontawesome-free'));
+app.use('/fontawesome', express.static('node_modules/@fortawesome/fontawesome-free/js'));
 app.use('/animate', express.static('node_modules/animate.css'));
 app.set('view engine', 'pug');
 const {error404, internalError} = require('./routes/errors');
